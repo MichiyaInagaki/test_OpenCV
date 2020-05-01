6056fe71792b7c000cfe7f22b59e7b9f1d2e4833
MichiyaInagaki <inagaki@sens.sys.es.osaka-u.ac.jp>
2020-05-01 10:37:37 +0900

    test

---
+++ opencv_test.cpp
@@ -0,0 +1,20 @@
+﻿// opencv_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(void)
+{
+	cv::Mat image;
+	image = cv::imread("C:/opencv/opencv_contrib/samples/data/corridor.jpg");
+
+	if (image.empty() == true) {
+		return 0;
+	}
+
+	cv::imshow("画像", image);
+	cv::waitKey();
+
+	return 0;
+}
